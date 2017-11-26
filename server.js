dd5da165816e52db06e6470a03bf83454936fae2
Binh Le <lvbinh09t4@gmail.com>
2017-11-26 08:36:46 +0700

    edit server.js to server index.html

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path')
 const serveStatic = require('serve-static')
 app = express()
 app.use(serveStatic(path.join(__dirname, '/dist')))
+app.get('*', (req, res) => {
+  res.sendfile('./dist/index.html')
+})
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
